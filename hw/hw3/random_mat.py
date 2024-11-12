6ec3fc9c9319a255db7ebf84975bdb5e815daf86
pietromalagoli <pietro.malagoli3@studio.unibo.it>
2024-11-12 16:16:17 +0100

    Ex3 done,only polish

--- hw/hw3/random_mat.py
+++ hw/hw3/random_mat.py
@@ -38,48 +38,117 @@ def real_diag(N):
     
     return D
 
-def spacingH(H):
+def spacingH(H, trim=True):
     
     # Compute and order the eigenvalues
     eigvals = np.linalg.eigvalsh(H) # Returns the eigenvalues already in ascending order
+    
+    if trim:
+        eigvals = eigvals[:eigvals.shape[0] - 1]
     
     spacing = []
     for i in range(eigvals.shape[0] - 1):       # - 1 otherwise index out-of-bounds
         s = eigvals[i+1] - eigvals[i]
         spacing.append(s)
     
-    spacing_norm = spacing / np.mean(spacing)
+    spacing_norm = spacing / np.max(spacing)
     
     return spacing_norm, eigvals
     
-def spacingD(D):
+def spacingD(D, trim=True):
     
     # Compute and order the eigenvalues
     eigvals = np.linalg.eigvals(D) # Returns the eigenvalues already in ascending order
     eigvals = np.sort(eigvals)
     
+    if trim:
+        eigvals = eigvals[:eigvals.shape[0] - 1]
+    
     spacing = []
     for i in range(eigvals.shape[0] - 1):       # - 1 otherwise index out-of-bounds
         s = eigvals[i+1] - eigvals[i]
         spacing.append(s)
     
-    spacing_norm = spacing / np.mean(spacing)
+    spacing_norm = spacing / np.max(spacing)
     
     return spacing_norm, eigvals
     
-# TEST
-'''
-print(spacingH(gen_herm(10)))
-print(spacingD(real_diag(10)))
-'''
 
 ########################## RANDOM MATRIX THEORY ###########################
+
+
+
+# THEORETICAL ANALYSIS
+
+# Wigner function
+import numpy as np
+
+def wigner(s, a, alpha, b, beta):
+    # s: spacing
+    
+    exponent = b * (s ** beta)
+    
+    # Check for large exponents
+    if np.any(exponent > 700):  # 700 is a safe threshold for most floating-point implementations
+        print('Exponent too large. Risk of overflow. Returning infinity.')
+        return np.inf  # or handle it in a way that makes sense for your application
+    
+    return a * (s ** alpha) * np.exp(exponent)
+
+def fitnplot(data, func, n_bins, mode, p0=[1,1,-1,1]):
+    
+    # Check for the right mode being input
+    if mode != 'Hermitian' and mode != 'Diagonal':
+        print('Mode not recognized. Sustained modes: \'Hermitian\', \'Diagonal\'.')
+        exit(1)
+
+    # Indicate the studied mode
+    print('MODE:', mode)
+
+    # Compute the range of the normalized spacing
+    deltaS = (np.min(data), np.max(data))
+    s_vals = np.linspace(deltaS[0], deltaS[1], 1000)
+
+    # Compute and normalize the counts for each bin for the two matrices
+    counts, bin_edges = np.histogram(data, bins=n_bins, range=deltaS, density=False)
+    bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
+
+    # Normalize counts
+    #counts_norm = counts / counts.sum()
+    
+    # Fit with the given function
+    params, params_cov = curve_fit(func, bin_centers, counts, p0=p0, maxfev=1000) # Pay attention to correctly initilialize the parameters (p0)
+
+    # Print the best parameters
+    print('BEST PARAMETERS retrieved from the fit:\n')
+    print('a:', params[0]); print('alpha:', params[1]); print('b:', params[2]); print('beta:', params[3])
+    
+    # Print the covariance matrix
+    print(f'COVARIANCE MATRIX:\n {params_cov}\n')
+    
+    
+    # Plotting
+    plt.figure(figsize=(8, 6))
+    plt.bar(bin_centers, counts, width=(bin_edges[1] - bin_edges[0]), color='orange', edgecolor='orange', alpha=0.7, label='Data')
+    plt.plot(s_vals, func(s_vals, params[0], params[1], params[2], params[3]), color='red', label='Fit')
+    plt.text(0.9, 0.7, f'a: {params[0]:.2f}\nalpha: {params[1]:.2f}\nb: {params[2]:.2f}\nbeta: {params[3]:.2f}', horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes, bbox=dict(facecolor='white', alpha=0.5)) # add a box for the parameters
+    plt.xlabel('Spacing (normalized)')
+    plt.ylabel('N events')
+    plt.title(f'Eigenvalue spacing distribution ({mode})')
+    plt.legend()
+    plt.grid()
+
+    plt.savefig(f'MatPlots/{mode}_mat.png')
+    #plt.show()
+    
+###### TEST
 
 # Size
 N = 1000
 
 # Set the seed
-np.random.seed(12345)
+#np.random.seed(12345)
+np.random.seed(11111)
 
 #  Full random hermitian matrix
 data_h, eig_r = spacingH(gen_herm(N))
@@ -91,90 +160,39 @@ data_d, eig_d = spacingD(real_diag(N))
 #print(data_d)
 #print(eig_d)
 
+test_plot = False
 
-#plt.figure(figsize=(10, 6))
-plt.hist(data_h[0:data_h.shape[0] - 1], bins=30, color="steelblue", edgecolor="black", alpha=0.4, label="Random Hermitian") # I neglect the first eigenvalue
-plt.xlabel('Spacing')
-plt.ylabel('Frequency')
-plt.title('Eigenvalue spacing distribution')
-plt.legend()
-plt.grid()
-
-# Save to file
-if not os.path.exists('MatPlots'):
-    os.makedirs('MatPlots')
-
-plt.savefig('MatPlots/herm_mat.png')
-plt.show()
-
-plt.hist(data_d, bins=30, color='orange', edgecolor='black', alpha=0.4, label='Diagonal')
-plt.xlabel('Spacing')
-plt.ylabel('Frequency')
-plt.title('Eigenvalue spacing distribution')
-plt.legend()
-plt.grid()
-
-plt.savefig('MatPlots/diag_mat.png')
-plt.show()
-
-# THEORETICAL ANALYSIS
-
-# Wigner function
-def wigner(s, a, alpha, b, beta):
-    # s: spacing
-    
-    return a*(s**alpha)*np.exp(b*(s**beta))
-
-def fitnplot(data, func, p0, n_bins, mode):
-    
-    # Check for the right mode being input
-    if mode != 'Hermitian' and mode != 'Diagonal':
-        print('Mode not recognized. Sustained modes: \'Hermitian\', \'Diagonal\'.')
-        exit(1)
-
-
-    # Compute the range of the normalized spacing
-    deltaS = (np.min(data), np.max(data))
-    s_vals = np.linspace(deltaS[0], deltaS[1], 1000)
-
-    # Compute and normalize the counts for each bin for the two matrices
-    counts, bin_edges = np.histogram(data, bins=n_bins, range=deltaS, density=False)
-    bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
-
-    # Calculate the probability distribution
-    #prob = counts / (N*deltaS)
-    
-    # Fit with the given function
-    params, params_cov = curve_fit(func, bin_centers, counts, p0=p0, maxfev=5000) # Pay attention to correctly initilialize the parameters (p0)
-
-    # Print the best parameters
-    print('BEST PARAMETERS retrieved from the fit:\n')
-    print('a:', params[0]); print('alpha:', params[1]); print('b:', params[2]); print('beta:', params[3]);
-    
-    # Print the covariance matrix
-    print(f'COVARIANCE MATRIX:\n {params_cov}\n')
-    
-    plt.figure(figsize=(8, 6))
-    plt.bar(bin_centers, counts, width=(bin_edges[1] - bin_edges[0]), color='orange', edgecolor='orange', alpha=0.7, label='Normalized histogram')
-
-    
-    # Plotting
-    #plt.hist(data, bins=n_bins, color='orange', edgecolor='orange', alpha=0.4, label='Data')
-    plt.plot(s_vals, func(s_vals, params[0], params[1], params[2], params[3]), color='red', label='Fit')
-    plt.text(0.9, 0.7, f'a: {params[0]:.2f}\nalpha: {params[1]:.2f}\nb: {params[2]:.2f}\nbeta: {params[3]:.2f}', horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes, bbox=dict(facecolor='white', alpha=0.5)) # add a box for the parameters
+if test_plot:
+    #plt.figure(figsize=(10, 6))
+    plt.hist(data_h, bins=30, color="steelblue", edgecolor="black", alpha=0.4, label="Random Hermitian") 
     plt.xlabel('Spacing')
     plt.ylabel('Frequency')
-    plt.title(f'Eigenvalue spacing distribution ({mode})')
+    plt.title('Eigenvalue spacing distribution')
     plt.legend()
     plt.grid()
 
-    plt.savefig(f'MatPlots/{mode}_mat.png')
+    # Save to file
+    if not os.path.exists('MatPlots'):
+        os.makedirs('MatPlots')
+
+    plt.savefig('MatPlots/herm_mat.png')
     plt.show()
-    
-# TEST
+
+    plt.hist(data_d, bins=30, color='orange', edgecolor='black', alpha=0.4, label='Diagonal')
+    plt.xlabel('Spacing')
+    plt.ylabel('Frequency')
+    plt.title('Eigenvalue spacing distribution')
+    plt.legend()
+    plt.grid()
+
+    plt.savefig('MatPlots/diag_mat.png')
+    plt.show()
 
 # Number of bins
 N_bins = 100
 
-fitnplot(data_h, wigner, [1,2,-2,1], N_bins, mode='Hermitian')
-fitnplot(data_d, wigner, [0.6,0.2,-2,0.6], N_bins, mode='Diagonal')
+#print('DATA HERMITIAN:\n',data_h)
+print('DATA DIAGONAL:\n',data_d)
+
+fitnplot(data_h, wigner, p0=[1,1,-1,1], n_bins=N_bins, mode='Hermitian')    # p0=[1,1,-1,1] 
+fitnplot(data_d, wigner, p0=[1,1,-1,1], n_bins=N_bins, mode='Diagonal')     # p0=[1,1,-1,1]
