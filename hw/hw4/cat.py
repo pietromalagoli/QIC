ebce26b1afee72885aa71a836e0825564bed1248
pietromalagoli <pietro.malagoli3@studio.unibo.it>
2024-11-14 13:58:34 +0100

    Ex4, eig to plot

--- hw/hw4/cat.py
+++ hw/hw4/cat.py
@@ -8,4 +8,154 @@
 #
 ###############################################################################################################################################
 
- 
+# IMPORTS
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.linalg import eigh_tridiagonal
+from typing import Union
+
+# VISUAL OPTIONS
+# print options for only 2 decimals
+np.set_printoptions(precision=2)
+
+### EIGENVALUES AND EIGENFUNCTIONS
+
+# Define the kinetic and potential matrices
+def K(a: Union[int, float], b: Union[int, float], N: int):
+    
+    ##  This function creates the kinetic matrix for the 1D harmonic oscillator
+    ##  (to the 2nd order) given the space discretization.
+    
+    # Check for acceptable values of the input data
+    if not isinstance(a, (int,float)):
+        raise TypeError('a must be a real number!')
+    
+    if not isinstance(b, (int,float)):
+        raise TypeError('b must be a real number!')
+    
+    if not isinstance(N, int):
+        raise TypeError('N must be an integer number!')
+    
+    if b < a:
+        print('b must be bigger than a.')
+        exit(1)
+    
+    # Define the spatial discretization
+    deltaX = (b - a) / N
+    
+    # Initialize the matrix
+    mat = np.zeros((N,N))
+    
+    # Fill the diagonal values
+    np.fill_diagonal(mat, 2)
+    
+    # Fill the lower and upper diagonal
+    for i in range(N-1):
+            mat[i,i+1] = -1
+            mat[i+1,i] = -1
+    
+    # Multiply by the coefficient
+    K = (1 / (2 * deltaX**2)) * mat
+
+    return K    
+
+def V(a: Union[int, float], b: Union[int, float], N: int, omega: Union[int,float]):
+    
+    ##  This function creates the potential matrix for the 1D harmonic oscillator
+    ##  (to the 2nd order) given the space discretization and 𝜔.
+    
+    # Check for acceptable values of the input data
+    if not isinstance(a, (int,float)):
+        raise TypeError('a must be a real number!')
+    
+    if not isinstance(b, (int,float)):
+        raise TypeError('b must be a real number!')
+    
+    if not isinstance(N, int):
+        raise TypeError('N must be an integer number!')
+    
+    if not isinstance(omega, (int,float)):
+        raise TypeError('omega must be a real number!')
+        
+    if b < a:
+        print('b must be bigger than a.')
+        exit(1)
+    
+    # Define the spatial discretization
+    deltaX = (b - a) / N
+    
+    # Define the spatial steps
+    x_vals = np.linspace(a + deltaX, b - deltaX, N)
+    
+    # Initialize the matrix
+    mat = np.zeros((N,N))
+    
+    # Fill the diagonal values
+    np.fill_diagonal(mat, x_vals)
+    
+    # Multiply by the coefficient
+    V = (omega**2 / 2)  * mat
+
+    return V
+
+def E_psi(K: np.array,V: np.array, k: int):
+    
+    ##  This function calculates the first k eigenvalues E_k
+    ##  and eigenfunctions Psi_k of the 1D harmonic oscillator 
+    ##  through the finite difference method at 2nd order.
+    
+    # Check for acceptable values of the input data
+    if K.shape != V.shape:
+        print('K and V must be square matrices of the same size.')
+        exit(1)
+    '''
+    if not isinstance(K, np.array):
+        raise TypeError('K must be a Numpy array!')
+    
+    if not isinstance(V, np.array):
+        raise TypeError('V must be a Numpy array!')
+    '''
+    if not isinstance(k, int):
+        raise TypeError('k must be an integer!')
+    
+    # Define the sum of the two matrices, S
+    S = K + V     # This will be a tridiagonal matrix
+    
+    # Calculate eigenvalues and eigenvectors
+    diag = np.zeros(K.shape[0])
+    off_diag = np.zeros(K.shape[0] - 1)
+    for i in range(K.shape[0]):
+        diag[i] = S[i,i]
+        if i < K.shape[0] - 1:          # this is necessary to avoid an index out of range error
+            off_diag[i] = S[i,i+1]
+    
+    # eigh_tridiagonal()'s select= and select_range= options let you compute up to the k-th eigenvalue (and eigenvectors)
+    eigvals, eigvec = eigh_tridiagonal(diag, off_diag, eigvals_only=False, select='i', select_range=(0,k))
+    
+    return eigvals, eigvec
+
+#def plot_eig(eigvals, eigvecs, a, b):
+    ##  This function plots the eigenvalues and eigenvectors of the 1D harmonic oscillator.
+    
+    
+
+## TEST
+
+# Define the interval [a,b] and the space discretization Deltax = (b - a) / N
+a = 0 
+b = 1
+N = 10
+omega = 2
+k = 5           # number of eigenvalues and eigenvetors to be computed
+
+K = K(a,b,N)
+V = V(a,b,N,omega)
+
+print(K)
+print(V)  
+
+eigvals, eigevecs = E_psi(K,V,k)
+print(eigvals)
+print(eigevecs)
+
+plot_eig(eigvals, eigevecs, a, b)
