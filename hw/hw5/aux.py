bb447e19d782318677dc213678779b5988efd788
pietromalagoli <pietro.malagoli3@studio.unibo.it>
2024-12-01 17:39:52 +0100

    Hw5-problems with normalization

--- hw/hw5/aux.py
+++ hw/hw5/aux.py
@@ -1,18 +1,22 @@
 ##########################################################################  
 #   This file contains generic auxiliary functions.
 
+#   IMPORT ZONE
+import numpy as np
+import scipy.integrate as integrate
+
 def checkpoint(debug: bool=False, verb: int=1, msg: str=None, var=None, stop: bool=False):
     """This function is used to debug the code. It prints a message and stops the execution if the stop flag is set to True.
 
     Args:
-        debug (bool, optional): _description_. Defaults to False.
-        verb (int, optional): _description_. Defaults to 1.
-        msg (str, optional): _description_. Defaults to None.
-        var (_type_, optional): _description_. Defaults to None.
-        stop (bool, optional): _description_. Defaults to False.
+        debug (bool, optional): sets the debug mode. Defaults to False.
+        verb (int, optional): level of verbosity. Defaults to 1.
+        msg (str, optional): optional message to print. Defaults to None.
+        var (_type_, optional): optional variable to print. Defaults to None.
+        stop (bool, optional): determines if the execution is stopped after the checkpoint. Defaults to False.
 
     Raises:
-        ValueError: _description_
+        ValueError: if the stop flag is set to True.
     """
     
     if debug:
@@ -35,12 +39,38 @@ def checkpoint(debug: bool=False, verb: int=1, msg: str=None, var=None, stop: bo
                 print(msg)
         if stop:
             raise ValueError(f'Execution stopped at checkpoint: {msg}')
-        
-def check_normalization(var):
-    
-    if sum(var) != 1:
-        raise ValueError(f'The variable {var} is not normalized.')
+'''    
+def check_normalization_wfc(wfc: np.array, space: np.array):
+    """This function checks if a wavefunction is normalized.
 
-               
+    Args:
+        wfc (np.array): wavefunction.
+        space (np.array): space over which the wavefunction is defined.
+
+    Raises:
+        ValueError: if the variable is not normalized.
+    """
     
+    if integrate.quad(opr.wfc) != 1:
+
+        checkpoint(debug=True, verb=2, msg=f'Variable not normalized', stop=True)               
+'''
+
+def check_normalization_wfc(wfc: np.array, momentum_space: bool=False, dk: float=None):
+    """This function checks if a wavefunction is normalized.
+
+    Args:
+        wfc (np.array): wavefunction.
+
+    Raises:
+        ValueError: if the variable is not normalized.
+    """
+    if momentum_space:
+        norm = np.sum(np.abs(wfc)**2)*dk
+    else:
+        norm = np.linalg.norm(wfc)
+        
+    if not np.isclose(norm, 1.0, atol=1e-6):
+
+        checkpoint(debug=True, verb=2, msg=f'Variable not normalized. Norm={np.linalg.norm(wfc)}', stop=True)     
             
